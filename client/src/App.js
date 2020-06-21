8ea22ffff259387fcfa8625ba916cbc48b69b5c1
studypurpose345 <studypurpose345@gmail.com>
2020-06-21 12:22:10 +0530

    added class in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <div className='App'>
         <header className='App-header'>
-          <h1>Fib Calculator</h1>
+          <h1 className='App-title'>Fib Calculator</h1>
           <Link to='/'>Home</Link>
           <Link to='/otherpage'>Other Page</Link>
         </header>
